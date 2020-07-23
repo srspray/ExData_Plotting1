084b7291d623028be81eaeff7d2cc393ee9e4758
srspray <66442565+srspray@users.noreply.github.com>
2020-07-23 10:07:09 -0500

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -3,7 +3,11 @@ data <- subset(fulldata, Date %in% c("1/2/2007","2/2/2007"))
 data$Date <- as.Date(data$Date, format="%d/%m/%Y")
 datetime <- paste(as.Date(data$Date), data$Time)
 data$Datetime <- as.POSIXct(datetime)
+## Plot 2
 with(data, {
   plot(Global_active_power~Datetime, type="l",
        ylab="Global Active Power (kilowatts)", xlab="")
 })
+## Save to file
+dev.copy(png, file="plot2.png", height=480, width=480)
+dev.off()
